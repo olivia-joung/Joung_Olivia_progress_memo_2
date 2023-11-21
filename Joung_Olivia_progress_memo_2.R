bdbf7a101a5b77110adbba02cb55fd0d60cda2cf
Olivia Joung <oliviajoung2025@u.northwestern.edu>
2023-11-21 16:01:35 -0600

    loaded in stats dataset

--- Joung_Olivia_progress_memo_2.R
+++ Joung_Olivia_progress_memo_2.R
@@ -2,14 +2,17 @@
 library(tidyverse)
 library(janitor)
 library(naniar)
+library(lubridate)
 
 ## Load data
 clues <- read_tsv("data/clues.tsv")
 puzzles <- read_tsv("data/puzzles.tsv")
-select <- read_tsv("data/similar.tsv")
+similar <- read_tsv("data/similar.tsv")
 
 
-# data cleaning (joining)
+## Data Cleaning
+
+# Cleaning/joining `clues` and `similar`
 clues_edited <- clues |> 
   mutate(
     pubid_year = paste0(pubid, year)
@@ -17,18 +20,34 @@ clues_edited <- clues |>
   relocate(pubid_year, .before = pubid) |> 
   arrange(year)
 
-clues_edited
-
-distinct_pubid_year <- puzzles |> 
+similar_edited <- similar |> 
   mutate(
-    pubid_year = str_remove_all(xdid, "-\\d{2}-\\d{2}")) |> 
-  relocate(pubid_year, .before = xdid) |> 
-  arrange(Date)
+    pubid_year = str_remove_all(xdid, "-\\d{2}-\\d{2}"),
+    year = as.integer(str_remove_all(pubid_year, "[a-zA-Z]+"))
+  ) |>
+  relocate(c(year, pubid_year), .before = xdid) |> 
+  group_by(pubid_year) |>
+  summarize(
+    reused_clues_mean = mean(reused_clues),
+    reused_answers_mean = mean(reused_answers),
+    total_clues_mean = mean(total_clues)
+  )
 
-distinct_pubid_year
+clues_edited |> 
+  left_join(similar_edited)
+
+# Cleaning puzzles
+puzzles_edited <- puzzles |> 
+  mutate(
+    pubid_year = str_remove_all(xdid, "-\\d{2}-\\d{2}"),
+    weekday = wday(Date, label = TRUE)) |> 
+  relocate(pubid_year, .before = xdid) |> 
+  arrange(Date) 
+
+puzzles_edited |> 
+    count(pubid_year == "atc1997")
   
-distinct_pubid_year |> 
-  left_join(clues_edited) |> 
-  select(pubid_year, answer, clue)
+
+  
 
 
