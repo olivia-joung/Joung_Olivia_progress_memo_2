f1e65522dab5ce3f87bd0f4778b6f12b436f18ec
Olivia Joung <oliviajoung2025@u.northwestern.edu>
2023-11-22 11:43:15 -0500

    loaded in english dictionary

--- Joung_Olivia_progress_memo_2.R
+++ Joung_Olivia_progress_memo_2.R
@@ -2,13 +2,12 @@
 library(tidyverse)
 library(janitor)
 library(naniar)
-library(lubridate)
 
 ## Load data
 clues <- read_tsv("data/clues.tsv")
 puzzles <- read_tsv("data/puzzles.tsv")
 similar <- read_tsv("data/similar.tsv")
-
+stats <- read_tsv("data/stats.tsv")
 
 ## Data Cleaning
 
@@ -33,7 +32,7 @@ similar_edited <- similar |>
     total_clues_mean = mean(total_clues)
   )
 
-clues_edited |> 
+clues_similar <- clues_edited |> 
   left_join(similar_edited)
 
 # Cleaning puzzles
@@ -44,10 +43,61 @@ puzzles_edited <- puzzles |>
   relocate(pubid_year, .before = xdid) |> 
   arrange(Date) 
 
-puzzles_edited |> 
-    count(pubid_year == "atc1997")
-  
+# Cleaning stats
+stats_edited <- stats |> 
+  filter(NumXd >= 1 & !is.na(NumXd)) |> 
+  arrange(year) |>
+  mutate(
+    pubid_year = paste0(pubid, year)
+  ) |> 
+  relocate(pubid_year, .before = pubid)
 
+# Joining stats with clues and puzzles 
+clues_joined <- clues_edited |> 
+  left_join(stats_edited |> select(pubid, year, weekday, Size, NumXd),
+            join_by(pubid, year),
+            relationship = "many-to-many")
+
+puzzles_joined <- puzzles_edited |> 
+  left_join(stats_edited |> select(pubid_year, weekday, Size, Editor, Copyright,
+                                   NumXd),
+            join_by(pubid_year, weekday, Size, Editor, Copyright),
+            relationship = "many-to-many") |> 
+  filter(!is.na(NumXd))
+
+#clues_joined |> 
+#  left_join(puzzles_joined, relationship = "many-to-many")
+
+## EDA
+
+# how many puzzles per year? - data will be skewed
+puzzles_joined |> 
+  mutate(
+    year = as.integer(str_remove_all(pubid_year, "[a-zA-Z]+"))
+  ) |> 
+  relocate(year, .before = pubid_year) |> 
+  group_by(year) |>
+  summarize(
+    n = n()
+  ) |> 
+  ggplot(aes(year)) +
+  geom_density() +
+  labs(
+    title = "Proportion of Puzzles by Year",
+    x = "Year"
+  )
   
+# most common clue by year?
+
+
+
+
+
+
+
+
+
+
+
 
 
